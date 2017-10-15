c59e44a18821c65bf269e99ee3b303e5fd0a24b6
Arash Yadegari <aryadegari@yahoo.com>
2017-10-15 17:03:25 +0330

    make sample text read in SparkTextReadSample

--- src/main/java/SparkTextReadSample.java
+++ src/main/java/SparkTextReadSample.java
@@ -2,44 +2,25 @@
  * Created by aryad on 14/10/2017.
  */
 
-import scala.Tuple2;
 
-import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.SparkSession;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import org.apache.spark.SparkConf;
 
 public class SparkTextReadSample {
 
-    private static final Pattern SPACE = Pattern.compile(" ");
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception{
+        String appName = "Length Count";
+        String master = "local";
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> lines = sc.textFile("./src/main/resources/sampleEventsLog");
+        JavaRDD<Integer> lineLengths = lines.map(String::length);
+        int totalLength = lineLengths.reduce((a, b) -> a + b);
 
-        if (args.length < 1) {
-            System.err.println("Usage: main.SparkTextReadSample <file>");
-            System.exit(1);
-        }
-
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("SparkTextReadSample")
-                .getOrCreate();
-
-        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-
-        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-
-        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?,?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
-        }
-        spark.stop();
+        System.out.println(totalLength);
     }
+
 }
+
